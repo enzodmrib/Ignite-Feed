9a6758f63ebca949a55662361225612310681efd
enzodmrib <enzorib2@gmail.com>
2022-06-28 00:42:58 -0300

    Splits Avatar into a separate component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
@@ -12,7 +13,7 @@ export function Sidebar() {
 
             <div className={styles.profile}>
 
-                <img className={styles.avatar} src="https://github.com/enzodmrib.png" />
+                <Avatar src="https://github.com/enzodmrib.png" />
 
                 <strong>Diego Fernandes</strong>
                 <span>Web Developer</span>
