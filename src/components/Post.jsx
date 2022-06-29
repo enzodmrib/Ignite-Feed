ac745d36703a1da15373bf8d1958af711fda8098
enzodmrib <enzorib2@gmail.com>
2022-06-29 15:22:51 -0300

    Adds remove comment functionality

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -34,6 +34,14 @@ export function Post({ author, publishedAt, content }) {
         setNewCommentText(event.target.value);
     }
 
+    function deleteComment(commentToDelete) {
+        const commentWithoutDeletedOne = comments.filter(comment => {
+            return comment !== commentToDelete;
+        });
+
+        setComments(commentWithoutDeletedOne);
+    }
+
     return (
         <article className={styles.post}>
             <header>
@@ -78,7 +86,7 @@ export function Post({ author, publishedAt, content }) {
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment key={comment} content={comment}/>
+                    return <Comment key={comment} content={comment} onDeleteComment={deleteComment}/>
                 })}
             </div>
         </article>
