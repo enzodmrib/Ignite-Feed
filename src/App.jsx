be1a85eb2f8bb12da248a26356ccd23b61878add
enzodmrib <enzorib2@gmail.com>
2022-06-29 05:00:51 -0300

    Adds comment uploading with custom text from state

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,37 @@ import styles from './App.module.css';
 
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/enzodmrib.png',
+      name: 'Enzo Ribeiro',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design / doctorcare' },
+    ],
+    publishedAt: new Date('2022-06-22 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://images.unsplash.com/photo-1557862921-37829c790f19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80',
+      name: 'Joe',
+      role: 'Mama'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design / doctorcare' },
+    ],
+    publishedAt: new Date('2022-06-28 20:00:00'),
+  }
+]
+
 function App() {
   return (
     <div>
@@ -14,14 +45,16 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Diego Fernandes"
-            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam ducimus labore architecto voluptates? Quas praesentium accusamus temporibus deleniti molestias ratione id tempore, magnam earum culpa reprehenderit illum, facere incidunt maxime."
-          />
-          <Post
-            author="Gabriel Buzzi"
-            content="novo post mouito legal"
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
